025e7274da0054dbdd6ed61f1a564ab39d0b197f
bmathus <bojko.matus@gmail.com>
2023-09-04 18:59:18 +0200

    mango connection

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
 const router = require('./routes/routes');
 
 //Mango connection
+mongoose.connect(process.env.DATABASE_URL);
+const database = mongoose.connection;
+
+database.on('error', (error) => {
+  console.log(error);
+});
+
+database.once('connected', () => {
+  console.log('Database Connected');
+});
 
 // Basic Configuration
 const app = express();
