0dab7517ff24312c205481cc0da65cb700fec447
bmathus <bojko.matus@gmail.com>
2023-09-05 14:27:59 +0200

    dns lookup

--- routes/routes.js
+++ routes/routes.js
@@ -2,46 +2,54 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const validUrl = require('valid-url');
+const dns = require('dns');
 const Model = require('../models/models');
 const { uid } = require('uid');
+const { error } = require('console');
 
 router.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '../', 'views', 'index.html'));
 });
 
 router.post('/api/shorturl', async (req, res) => {
-  const url = req.body.url;
+  const fullurl = req.body.url;
 
-  if (!validUrl.isUri(url)) {
+  if (!validUrl.isUri(fullurl)) {
     return res.json({ error: 'invalid url' });
   }
 
-  const record = await Model.findOne({
-    original_url: url,
-  });
+  const u = new URL(fullurl);
+  dns.lookup(u.hostname, 4, async (error, address, family) => {
+    if (error) {
+      return res.json({ error: 'invalid url' });
+    }
 
-  if (!record) {
-    const shortUrl = uid(6);
-
-    const newRecord = new Model({
-      original_url: url,
-      short_url: shortUrl,
+    const record = await Model.findOne({
+      original_url: fullurl,
     });
 
-    try {
-      const recordToSave = await newRecord.save();
-      return res.json({
-        original_url: recordToSave.original_url,
-        short_url: recordToSave.short_url,
+    if (!record) {
+      const shortUrl = uid(6);
+      const newRecord = new Model({
+        original_url: fullurl,
+        short_url: shortUrl,
       });
-    } catch (error) {
-      return res.json({ message: error.message });
-    }
-  }
 
-  return res.json({
-    original_url: url,
-    short_url: record.short_url,
+      try {
+        const recordToSave = await newRecord.save();
+        return res.json({
+          original_url: recordToSave.original_url,
+          short_url: recordToSave.short_url,
+        });
+      } catch (error) {
+        return res.json({ message: error.message });
+      }
+    }
+
+    return res.json({
+      original_url: fullurl,
+      short_url: record.short_url,
+    });
   });
 });
 
